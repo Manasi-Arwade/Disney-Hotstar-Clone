ed8d44da4735c203086abb30fdcf6aaa8d43741e
Manasi-Arwade <97795724+Manasi-Arwade@users.noreply.github.com>
2022-07-09 14:50:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,31 +3,31 @@ let movies = [
     name: "falcon and the winter soldier",
     des:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
-    image: "images/slider 2.png"
+    image: "slider 2.png"
   },
   {
     name: "loki",
     des:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
-    image: "images/slider 1.png"
+    image: "slider 1.png"
   },
   {
     name: "wanda vision",
     des:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
-    image: "images/slider 3.png"
+    image: "slider 3.png"
   },
   {
     name: "raya and the last dragon",
     des:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
-    image: "images/slider 4.png"
+    image: "slider 4.png"
   },
   {
     name: "luca",
     des:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
-    image: "images/slider 5.png"
+    image: "slider 5.png"
   }
 ];
 
